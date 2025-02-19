9c0965db935ad4834f55a991eee9fa5140cbd1d3
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-02-18 21:12:35 -0300

    Iniciando conteúdos do desenvolvimento Mobile utilizando o React Native o App: Expo GO

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Aluno Teixeira Leonardo!</Text>
+      <Text style={styles.text}>Turma 2TDSPW</Text>
+      <Image
+        source={require("./assets/splash-icon.png")}
+        style={{ width: 200, height: 200 }}
+      />
+      <TextInput style={styles.input} placeholder="Digite seu nome:" />
+      <Button title="isso é um botao" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 36,
+    fontWeight: 600,
+  },
+  input: {
+    backgroundColor: "#fff",
+    padding: 10, // Ajustado para um valor razoável
+    width: 300,
+    borderRadius: 8, // Opcional: arredonda a borda do input
   },
 });
